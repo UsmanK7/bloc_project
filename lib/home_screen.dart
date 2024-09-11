b62ffe1afdabe323406d17a8fa079ccb9861ac03
Usman Khan <95441787+UsmanK7@users.noreply.github.com>
2024-09-12 03:55:29 +0500

    fafe

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Home Screen',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: Expanded(
+          child: ListView.builder(
+              itemCount: 7,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  trailing: const Icon(Icons.delete),
+                  title: Text('Item $index'),
+                );
+              })),
+    );
   }
 }
